f352a92bcbaec50c42906c91b477f612a946fe92
alexm <alexmamo@gmail.com>
2021-02-02 06:02:40 +0200

    Dependency updates.

--- app/src/main/java/ro/alexmamo/firestorepagination/products/ProductsViewModel.kt
+++ app/src/main/java/ro/alexmamo/firestorepagination/products/ProductsViewModel.kt
@@ -1,15 +1,17 @@
 package ro.alexmamo.firestorepagination.products
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import com.google.firebase.firestore.Query
+import dagger.hilt.android.lifecycle.HiltViewModel
 import ro.alexmamo.firestorepagination.utils.Constants.PAGE_SIZE
+import javax.inject.Inject
 
-class ProductsViewModel @ViewModelInject constructor(
+@HiltViewModel
+class ProductsViewModel @Inject constructor(
     private val queryProductsByName: Query
 ) : ViewModel() {
     val flow = Pager(
